c72ff9a0132956a1a6a08c69809bba739b289629
SophiaLM <106804291+SophiaLM@users.noreply.github.com>
2026-05-06 15:52:17 +0200

    Delete ft_alembic_2.py

--- ft_alembic_2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import alchemy.elements
-
-print("=== Alembic 2 ===")
-print("Accessing alchemy/elements.py using 'import ...' structure")
-eart = alchemy.elements.create_earth()
-print(f"Testing create_earth: {eart}")
